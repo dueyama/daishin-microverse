2b804b2f460c4354df87560a24e816b83ae5540c
dueyama <98719466+dueyama@users.noreply.github.com>
2022-11-08 12:23:38 +0900

    Initial commit

---
+++ apiKey.js
@@ -0,0 +1,12 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1tNFBeyAFHvGjke4JP7adMo8qKjhJxtoLDk11BCpD';
+const appId = 'com.gmail.d.ueyama.microverse';
+export default {apiKey, appId};
+
+// For development on local computer and local network, use a development key
+// from croquet.io/keys and create a file called apiKey-dev.js.
+
+// For deployment to a publicly accessible server, create a production key
+// from croquet.io/keys and create a file called apiKey.js.
+
+// you may export other Croquet session parameters to override default values.
