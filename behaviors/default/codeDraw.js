f360eb77c82399281b3fbcf94af7fcd82a533194
dueyama <98719466+dueyama@users.noreply.github.com>
2022-11-08 13:05:31 +0900

    Add codeDraw.js

---
+++ behaviors/default/codeDraw.js
@@ -0,0 +1,37 @@
+class CodeDrawActor {
+    setup() {
+        console.log("CodeDrawActor.setup");
+    }
+}
+
+class CodeDrawPawn {
+    setup() {
+        console.log("CodeDrawPawn.setup");
+    }
+
+    resize(width, height) {
+        console.log(width, height);
+    }
+
+    clear() {
+        console.log("CLEAR")
+        let canvas = this.canvas;
+        let ctx = canvas.getContext('2d');
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        this.texture.needsUpdate = true;
+    }
+}
+
+export default {
+    modules: [
+        {
+            name: "DrawingCanvas",
+            actorBehaviors: [CodeDrawActor],
+            pawnBehaviors: [CodeDrawPawn],
+        }
+    ]
+};
+
+/* globals Microverse */
+
